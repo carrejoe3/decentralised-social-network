23febba6681b0f228fffcc67fc8efd0faad49e3e
Joe <carre.joe3@gmail.com>
2020-08-02 16:07:19 +0100

    split navbar into its own component

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Web3 from 'web3';
 import logo from '../logo.png';
 import './App.css';
+import Navbar from './Navbar';
 
 class App extends Component {
 
@@ -47,24 +48,10 @@ class App extends Component {
     this.setState({ account: accounts[0] })
   }
 
-  render() {
+  render () {
     return (
       <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Dapp University
-          </a>
-          <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
-            <small className="text-secondary">
-              <small id="account">{ this.state.account }</small>
-            </small>
-          </li>
-        </nav>
+        <Navbar account={ this.state.account } />
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
