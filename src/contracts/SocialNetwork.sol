2544c6c7a4862cb078f94c257c5365c40082a972
Joe <carre.joe3@gmail.com>
2020-08-02 11:10:04 +0100

    createPost function and associated tests

--- src/contracts/SocialNetwork.sol
+++ src/contracts/SocialNetwork.sol
@@ -2,8 +2,36 @@ pragma solidity ^0.5.0;
 
 contract SocialNetwork {
   string public name;
+  uint public postCount = 0;
+
+  mapping (uint => Post) public posts;
+
+  struct Post {
+    uint id;
+    string content;
+    uint tipAmount;
+    address author;
+  }
+
+  event PostCreated (
+    uint id,
+    string content,
+    uint tipAmount,
+    address author
+  );
 
   constructor () public {
     name = "Social Network";
+  }
+
+  function createPost (string memory _content) public {
+    // Require valid content
+    require(bytes(_content).length > 0, 'post must have content');
+    // Increment post count
+    postCount ++;
+    // Create the post
+    posts[postCount] = Post(postCount, _content, 0, msg.sender);
+    // Trigger event
+    emit PostCreated(postCount, _content, 0, msg.sender);
   }
 }
--- test/SocialNetwork.js
+++ test/SocialNetwork.js
@@ -6,7 +6,7 @@ require('chai')
   .use(require('chai-as-promised'))
   .should()
 
-contract ('SocialNetwork', (accounts) => {
+contract ('SocialNetwork', ([deployer, author, tipper]) => {
   let socialNetwork
 
   before(async () => {
@@ -25,6 +25,36 @@ contract ('SocialNetwork', (accounts) => {
     it('has a name', async () => {
       const name = await socialNetwork.name()
       assert.equal(name, 'Social Network')
+    })
+  })
+
+  describe('posts', async () => {
+    let result, postCount
+
+    it('creates posts', async () => {
+      result = await socialNetwork.createPost('This is a post', { from: author })
+      postCount = await socialNetwork.postCount()
+
+      // SUCCESS
+      assert.equal(postCount, 1)
+
+      const event = result.logs[0].args
+
+      assert.equal(event.id.toNumber(), postCount.toNumber(), 'id is correct')
+      assert.equal(event.content, 'This is a post', 'content is correct')
+      assert.equal(event.tipAmount, '0', 'tip amount is correct')
+      assert.equal(event.author, author, 'author is correct')
+
+      // FAIL: posts must have content
+      result = await socialNetwork.createPost('', { from: author }).should.be.rejected;
+    })
+
+    it('lists posts', async () => {
+
+    })
+
+    it('allows users to tip posts', async () => {
+
     })
   })
 })
