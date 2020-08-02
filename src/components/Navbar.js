2a1f68659615fcabe08ce8036fb0651ebf268e58
Joe <carre.joe3@gmail.com>
2020-08-02 16:16:45 +0100

    added an identicon profile logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Identicon from 'identicon.js';
 
 class Navbar extends Component {
   render () {
@@ -16,6 +17,16 @@ class Navbar extends Component {
           <small className="text-secondary">
             <small id="account">{ this.props.account }</small>
           </small>
+          { this.props.account
+            ? <img
+              className="ml-2"
+              width="30"
+              height="30"
+              src={ `data:image/png;base64,${new Identicon(this.props.account, 30).toString()}` }
+              alt="Profile icon"
+            />
+            : <span></span>
+          }
         </li>
       </nav>
     );
